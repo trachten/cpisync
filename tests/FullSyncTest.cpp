1f2fc46ac56adf7e24bcb3703f8a3b8e6927c96e
Eliezer Pearl <eliezer.pearl@gmail.com>
2018-07-18 14:17:40 -0400

    temp change

--- tests/FullSyncTest.cpp
+++ tests/FullSyncTest.cpp
@@ -100,74 +100,132 @@ void FullSyncTest::basicTest() {
 }
 
 void FullSyncTest::thoroughTest() {
-    const int TIMES = 100;
-    for(int ii = 0; ii < TIMES; ii++) {
-        vector<DataObject *> objectsPtr;
+//    const int TIMES = 100;
+//    for(int ii = 0; ii < TIMES; ii++) {
+//        vector<DataObject *> objectsPtr;
+//
+//        GenSync GenSyncServer = GenSync::Builder().
+//                setProtocol(GenSync::SyncProtocol::FullSync).
+//                setComm(GenSync::SyncComm::socket).
+//                build();
+//        GenSync GenSyncClient = GenSync::Builder().
+//                setProtocol(GenSync::SyncProtocol::FullSync).
+//                setComm(GenSync::SyncComm::socket).
+//                build();
+//
+//        const int SHARED = rand() % 50;
+//        const int CLIENT_MINUS_SERVER = rand() % 25;
+//        const int SERVER_MINUS_CLIENT = rand() % 25;
+//
+//        for(int jj=0; jj<SHARED; jj++) {
+//            DataObject* obj = randDO();
+//            GenSyncClient.addElem(obj);
+//            GenSyncServer.addElem(obj);
+//            objectsPtr.push_back(obj);
+//        }
+//
+//        for(int jj=0; jj<CLIENT_MINUS_SERVER; jj++) {
+//            DataObject* obj = randDO();
+//            GenSyncClient.addElem(obj);
+//            objectsPtr.push_back(obj);
+//        }
+//
+//        for(int jj=0; jj<SERVER_MINUS_CLIENT; jj++) {
+//            DataObject* obj = randDO();
+//            GenSyncServer.addElem(obj);
+//            objectsPtr.push_back(obj);
+//        }
+//
+//        forkHandleReport resultServer = forkHandle(GenSyncServer, GenSyncClient);
+//        forkHandleReport resultClient = forkHandle(GenSyncClient, GenSyncServer);
+//        // check that the result is correct
+//        // ... statistics are reasonable
+//        CPPUNIT_ASSERT(resultServer.bytes > 0);
+//        CPPUNIT_ASSERT(resultClient.bytes > 0);
+//        //CPPUNIT_ASSERT(resultServer.CPUtime > 0); // CPUtime could be 0 if this test is fast enough
+//        //CPPUNIT_ASSERT(resultClient.CPUtime > 0);
+//        //CPPUNIT_ASSERT(resultServer.totalTime > 0);
+//        //CPPUNIT_ASSERT(resultClient.totalTime > 0);
+//        CPPUNIT_ASSERT(resultServer.success); // i.e. successful synchronization
+//        CPPUNIT_ASSERT(resultClient.success);
+//
+//        // ... that server and client have all the data
+//        auto serverElems = GenSyncServer.dumpElements();
+//        auto clientElems = GenSyncClient.dumpElements();
+//        multiset<string> serverElemsStr, clientElemsStr; // multisets of string representations of the corresponding DataObjects
+//
+//        for (auto elem : serverElems) {
+//            serverElemsStr.insert(elem->print());
+//            cout << "Server:" << elem->print() << endl;
+//        }
+//        for (auto elem : clientElems) {
+//            clientElemsStr.insert(elem->print());
+//            cout << "Client:" << elem->print() << endl;
+//        }
+//
+//        CPPUNIT_ASSERT(serverElemsStr.size() > 0);
+//        auto e = multisetDiff(clientElemsStr, serverElemsStr);
+//        CPPUNIT_ASSERT(multisetDiff(serverElemsStr, clientElemsStr).empty());
+//        CPPUNIT_ASSERT(multisetDiff(clientElemsStr, serverElemsStr).empty());
+//        CPPUNIT_ASSERT_EQUAL(serverElemsStr.size(), clientElemsStr.size());
+//
+//    }
+    // setup DataObjects
+    const unsigned long DIFF = 50; // amt of elements shared between client and server
 
-        GenSync GenSyncServer = GenSync::Builder().
-                setProtocol(GenSync::SyncProtocol::FullSync).
-                setComm(GenSync::SyncComm::socket).
-                build();
-        GenSync GenSyncClient = GenSync::Builder().
-                setProtocol(GenSync::SyncProtocol::FullSync).
-                setComm(GenSync::SyncComm::socket).
-                build();
-
-        const int SHARED = rand() % 50;
-        const int CLIENT_MINUS_SERVER = rand() % 25;
-        const int SERVER_MINUS_CLIENT = rand() % 25;
-
-        for(int jj=0; jj<SHARED; jj++) {
-            DataObject* obj = randDO();
-            GenSyncClient.addElem(obj);
-            GenSyncServer.addElem(obj);
-            objectsPtr.push_back(obj);
-        }
-
-        for(int jj=0; jj<CLIENT_MINUS_SERVER; jj++) {
-            DataObject* obj = randDO();
-            GenSyncClient.addElem(obj);
-            objectsPtr.push_back(obj);
-        }
-
-        for(int jj=0; jj<SERVER_MINUS_CLIENT; jj++) {
-            DataObject* obj = randDO();
-            GenSyncServer.addElem(obj);
-            objectsPtr.push_back(obj);
-        }
-
-        forkHandleReport resultServer = forkHandle(GenSyncServer, GenSyncClient);
-        forkHandleReport resultClient = forkHandle(GenSyncClient, GenSyncServer);
-        // check that the result is correct
-        // ... statistics are reasonable
-        CPPUNIT_ASSERT(resultServer.bytes > 0);
-        CPPUNIT_ASSERT(resultClient.bytes > 0);
-        //CPPUNIT_ASSERT(resultServer.CPUtime > 0); // CPUtime could be 0 if this test is fast enough
-        //CPPUNIT_ASSERT(resultClient.CPUtime > 0);
-        //CPPUNIT_ASSERT(resultServer.totalTime > 0);
-        //CPPUNIT_ASSERT(resultClient.totalTime > 0);
-        CPPUNIT_ASSERT(resultServer.success); // i.e. successful synchronization
-        CPPUNIT_ASSERT(resultClient.success);
-
-        // ... that server and client have all the data
-        auto serverElems = GenSyncServer.dumpElements();
-        auto clientElems = GenSyncClient.dumpElements();
-        multiset<string> serverElemsStr, clientElemsStr; // multisets of string representations of the corresponding DataObjects
-
-        for (auto elem : serverElems) {
-            serverElemsStr.insert(elem->print());
-            cout << "Server:" << elem->print() << endl;
-        }
-        for (auto elem : clientElems) {
-            clientElemsStr.insert(elem->print());
-            cout << "Client:" << elem->print() << endl;
-        }
-
-        CPPUNIT_ASSERT(serverElemsStr.size() > 0);
-        auto e = multisetDiff(clientElemsStr, serverElemsStr);
-        CPPUNIT_ASSERT(multisetDiff(serverElemsStr, clientElemsStr).empty());
-        CPPUNIT_ASSERT(multisetDiff(clientElemsStr, serverElemsStr).empty());
-        CPPUNIT_ASSERT_EQUAL(serverElemsStr.size(), clientElemsStr.size());
-
+    vector<DataObject *> objectsPtr;
+    for (unsigned long ii = 0; ii < DIFF * 2; ii++) {
+        objectsPtr.push_back(randDO());
     }
+
+    // Build server
+    GenSync GenSyncServer = GenSync::Builder().
+            setProtocol(GenSync::SyncProtocol::FullSync).
+            setComm(GenSync::SyncComm::socket).
+            build();
+    // ... add data objects unique to the server
+    for (auto iter = objectsPtr.begin(); iter != objectsPtr.begin() + DIFF; iter++) {
+        GenSyncServer.addElem(*iter);
+    }
+
+    // Build client
+    GenSync GenSyncClient = GenSync::Builder().
+            setProtocol(GenSync::SyncProtocol::FullSync).
+            setComm(GenSync::SyncComm::socket).
+            build();
+    // ... add data objects unique to the client
+    for (auto iter = objectsPtr.begin() + DIFF; iter != objectsPtr.end(); iter++) {
+        GenSyncClient.addElem(*iter);
+    }
+
+    // create separate processes for the server and client
+    forkHandleReport resultServer = forkHandle(GenSyncServer, GenSyncClient);
+    forkHandleReport resultClient = forkHandle(GenSyncClient, GenSyncServer);
+    // check that the result is correct
+    // ... statistics are reasonable
+    CPPUNIT_ASSERT(resultServer.bytes > 0);
+    CPPUNIT_ASSERT(resultClient.bytes > 0);
+    //CPPUNIT_ASSERT(result.CPUtime > 0); // CPUtime could be 0 if this test is fast enough
+    //CPPUNIT_ASSERT(result.totalTime > 0);
+    CPPUNIT_ASSERT(resultServer.success); // i.e. successful synchronization
+    CPPUNIT_ASSERT(resultClient.success);
+
+    // ... that server and client have all the data
+    auto serverElems = GenSyncServer.dumpElements();
+    auto clientElems = GenSyncClient.dumpElements();
+    multiset<string> serverElemsStr, clientElemsStr; // multisets of string representations of the corresponding DataObjects
+
+    for (auto elem : serverElems) {
+        serverElemsStr.insert(elem->print());
+        cout << "Server:" << elem->print() << endl;
+    }
+    for (auto elem : clientElems) {
+        clientElemsStr.insert(elem->print());
+        cout << "Client:" << elem->print() << endl;
+    }
+
+    CPPUNIT_ASSERT(!serverElemsStr.empty());
+    CPPUNIT_ASSERT_EQUAL(serverElemsStr.size(), clientElemsStr.size());
+    CPPUNIT_ASSERT(multisetDiff(serverElemsStr, clientElemsStr).empty());
+    CPPUNIT_ASSERT(multisetDiff(clientElemsStr, serverElemsStr).empty());
 }

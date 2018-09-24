d4723b7984f3866b8a36a3088eda872e9a2ac9b6
bowen <bowenislandsong@gmail.com>
2018-09-24 16:17:56 -0400

    commented out

--- tests/unit/kshinglingSyncTest.cpp
+++ tests/unit/kshinglingSyncTest.cpp
@@ -22,35 +22,35 @@ void kshinglingSyncTest::tearDown() {}
 
 void kshinglingSyncTest::testAll(){
 
-    int editDistance_bar = 5;
-    int shingle_len = 3;
-    kshinglingSync(GenSync::SyncProtocol::InteractiveCPISync, 5,8,3);
-
-    const int EXP_ELEM = UCHAR_MAX * 2;
-
-    GenSync GenSyncServer = GenSync::Builder().
-            setProtocol(GenSync::SyncProtocol::IBLTSync).
-            setComm(GenSync::SyncComm::socket).
-            setBits(8).
-            setNumExpectedElements(EXP_ELEM).
-            build();
-
-    GenSync GenSyncClient = GenSync::Builder().
-            setProtocol(GenSync::SyncProtocol::IBLTSync).
-            setComm(GenSync::SyncComm::socket).
-            setBits(8).
-            setNumExpectedElements(EXP_ELEM).
-            build();
-
-    string Alicetxt = genRandString(100);  // generate a string
-    string Bobtxt = randStringEdit(Alicetxt,10);  // Generate a edited string
-    GenSyncServer.addElem()
-    K_Shingle Alice = K_Shingle(Alicetxt, 3);  // Init k shingling on Alice
-    auto aliceset = Alice.getShingleSet();
-    for (int i = 0; i < aliceset.size(); ++i) {
-
-    }
-    syncTest(GenSyncServer, GenSyncClient);
-
-    auto AliceStrNOrder = Alice.reconstructStringBacktracking();  // Get order of the cycle
+//    int editDistance_bar = 5;
+//    int shingle_len = 3;
+//    kshinglingSync(GenSync::SyncProtocol::InteractiveCPISync, 5,8,3);
+//
+//    const int EXP_ELEM = UCHAR_MAX * 2;
+//
+//    GenSync GenSyncServer = GenSync::Builder().
+//            setProtocol(GenSync::SyncProtocol::IBLTSync).
+//            setComm(GenSync::SyncComm::socket).
+//            setBits(8).
+//            setNumExpectedElements(EXP_ELEM).
+//            build();
+//
+//    GenSync GenSyncClient = GenSync::Builder().
+//            setProtocol(GenSync::SyncProtocol::IBLTSync).
+//            setComm(GenSync::SyncComm::socket).
+//            setBits(8).
+//            setNumExpectedElements(EXP_ELEM).
+//            build();
+//
+//    string Alicetxt = genRandString(100);  // generate a string
+//    string Bobtxt = randStringEdit(Alicetxt,10);  // Generate a edited string
+//    GenSyncServer.addElem()
+//    K_Shingle Alice = K_Shingle(Alicetxt, 3);  // Init k shingling on Alice
+//    auto aliceset = Alice.getShingleSet();
+//    for (int i = 0; i < aliceset.size(); ++i) {
+//
+//    }
+//    syncTest(GenSyncServer, GenSyncClient);
+//
+//    auto AliceStrNOrder = Alice.reconstructStringBacktracking();  // Get order of the cycle
 }

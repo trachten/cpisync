8aaa2b62849dcd1fffd80145a5f2146295de25bf
bowen <bowenislandsong@gmail.com>
2018-10-22 16:08:31 -0400

    Strata

--- include/StrataEst.h
+++ include/StrataEst.h
@@ -20,6 +20,7 @@
 #include <string>
 #include <NTL/ZZ.h>
 #include <sstream>
+#include "DataObject.h"
 #include "IBLT.h"
 #include "Auxiliary.h"
 
@@ -33,33 +34,84 @@ using namespace NTL;
 
 typedef unsigned long int hashVal;
 
-typedef vector<IBLT> IBF;
-
 // Preset IBF Parameters
-const int idSumByteSize = 4, hashSumByteSize = 4, countByteSize = 4;
+const size_t idSumByteSize = 4, hashSumByteSize = 4, countByteSize = 4;
 
 
 // use H_z to hash each set element as we sample
 
-// 16 / 32 number of strata and 80 cells by default
+// 16 / 32 number of strata and 40 / 80 cells by default
 
 class StrataEst{
 public:
 
-    friend class Communicant;
-
-    StrataEst(int num_strata, int num_cells, int value_size);
+    /**
+     * Construct a vector of IBF Hierarchy as Strata
+     * @param num_strata default value 16 / 32
+     * @param num_cells default value 40 / 80
+     * @param value_size default value 8
+     */
+    StrataEst( size_t value_size, size_t num_strata = 16, size_t num_cells=40);
 
     ~StrataEst();
 
-    void insert(ZZ item);
+    void insert(DataObject * datum);
 
-    void estimate(IBF myIBF);
+    /**
+     * Estimate the set difference by examine subtracted Strata
+     * @return Estimated set difference
+     */
+    size_t estimate();
+
+    /**
+     * Sample from set and pour into Strata Estimator
+     * @return Strata Estimator
+     */
+    vector<IBLT> exportStrata();
+
+    /**
+     * Subtracts two StrataEst.
+     * -= is destructive and assigns the resulting StrataEst to the value, whereas - isn't. -= is more efficient than -
+     * @require StrataEst must have the same number of Stratas, entries, and the values must be of the same size
+     */
+    StrataEst operator-(const StrataEst& other) const;
+    StrataEst& operator-=(const StrataEst& other);
+
+    /**
+     * @return the number of cells in an IBF.
+     */
+    size_t IBFsize() const;
+
+    /**
+     * @return the number of IBFs in the Strata.
+     */
+    size_t size() const;
+
+    /**
+     * @return the size of a value stored in the IBF.
+     */
+    size_t eltSize() const;
 
 private:
-    int numStrata, numCells, bits;
 
+    // assume all inputs are unique.
+    void _insert(ZZ item);
 
+    // inject the element into the strata for a specific IBLT
+    void _inject(ZZ item, int index);
+
+    /**
+     * Get the index of the IBF what this Elem is assigned to. The bigger the number less the chance
+     * @param Elem
+     * @return index of the assignent for the IBF from 0
+     */
+    int get_Assign_Ind(ZZ& Elem);
+
+    size_t numStrata, numCells, bits;
+
+    vector<IBLT> Strata;
+
+    vector<ZZ> setElems;
 
 };
 
--- src/StrataEst.cpp
+++ src/StrataEst.cpp
@@ -4,3 +4,58 @@
 
 #include "StrataEst.h"
 
+StrataEst::StrataEst(size_t num_strata, size_t num_cells, size_t value_size) {
+    numStrata =  num_strata, numCells =  num_cells, bits =  value_size;
+    for (int i = 0; i < numStrata; ++i) {
+        IBLT iblt = IBLT(numCells,bits);
+        Strata.push_back(iblt);
+    }
+}
+
+void StrataEst::insert(DataObject *datum) {
+    _insert(datum->to_ZZ());
+}
+
+void StrataEst::_insert(ZZ Elem) {
+    setElems.push_back(Elem);
+}
+
+vector<IBLT> StrataEst::exportStrata() {
+    // feed all element rough a hash function get their designated IBF
+    for (ZZ elem : setElems) {
+        //put the element into the designated IBF
+        _inject(elem, get_Assign_Ind(elem));
+    }
+    return Strata;
+}
+
+int StrataEst::get_Assign_Ind(ZZ& Elem) {
+    std::hash<std::string> shash;
+    size_t space = pow(2,numStrata);
+    return ceil((shash(toStr(Elem)) % space)/2);
+}
+
+void StrataEst::_inject(ZZ item, int index) {
+    IBLT &iblt = Strata.at(index);
+    iblt.insert(item, item);
+}
+
+size_t StrataEst::estimate() {
+    size_t count = 0;
+    for (int i = 0; i < Strata.size(); ++i) {
+        vector<pair<ZZ, ZZ>> pos, neg;
+        bool success = Strata[i].listEntries(pos, neg);
+        count += pos.size()+neg.size();
+        if (success){
+            count *= (size_t) pow(2,i+1);
+            return count;
+        }
+    }
+    return
+}
+
+size_t StrataEst::IBFsize() const { return numCells; }
+
+size_t StrataEst::size() const { return numStrata; }
+
+size_t StrataEst::eltSize() const { return bits; }

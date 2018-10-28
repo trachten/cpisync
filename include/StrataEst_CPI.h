0be01505d3ac3655addc793d4a15b808256e7ec9
bowenislandsong <bowenislandsong@gmail.com>
2018-10-27 20:14:53 -0400

    CMake

---
+++ include/StrataEst_CPI.h
@@ -0,0 +1,21 @@
+//
+// Created by Bowen Song on 10/26/18.
+//
+
+#ifndef CPISYNCLIB_STRATAEST_CPI_H
+#define CPISYNCLIB_STRATAEST_CPI_H
+
+#include "Auxiliary.h"
+#include "Communicant.h"
+#include "Exceptions.h"
+#include "Logger.h"
+#include "DataObject.h"
+#include "CPISync.h"
+#include "InterCPISync.h"
+#include "CPISync_HalfRound.h"
+#include "CPISync_HalfRound_Hashed.h"
+#include "CPISync_ExistingConnection.h"
+
+
+
+#endif //CPISYNCLIB_STRATAEST_CPI_H
---
+++ src/StrataEst_CPI.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Bowen Song on 10/26/18.
+//
+
+#include "StrataEst_CPI.h"
+

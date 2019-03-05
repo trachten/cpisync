ec9e491f1712414026192d4934a3d7459ab1e6c3
sean-brandenburg <seanbrandenburg333@gmail.com>
2019-03-05 00:07:53 -0500

    implemented CommSocketTest

---
+++ tests/unit/CommSocketTest.h
@@ -0,0 +1,32 @@
+#ifndef COMMSOCKETTEST_H
+#define COMMSOCKETTEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class CommSocketTest : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(CommSocketTest);
+
+    CPPUNIT_TEST(GetSocketInfo);
+    CPPUNIT_TEST(SocketSendAndRecieveTest);
+
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    CommSocketTest();
+    ~CommSocketTest() override;
+
+    void setUp() override;
+    void tearDown() override;
+
+private:
+  	// Tests getName and getHost functions ensuring that constructor is working as intended
+    void GetSocketInfo();
+
+	// Tests sending and recieving a string of characters through a socket
+    void SocketSendAndRecieveTest();
+
+};
+
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( CommSocketTest, CommSocketTest );
+
+#endif /* COMMSOCKETTEST_H */

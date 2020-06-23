5e5b091bf3387101fa3a7a30b3ff762915256e46
Shubham Arora <aroras@bu.edu>
2020-06-23 16:46:08 -0400

    Run Sync with IBLTSync and CPISync as: 1. load client & server data from file 2. run client process, server process 3. print stats about sync

---
+++ src/TryMultisetIBLTSync.cpp
@@ -0,0 +1,181 @@
+//
+// Created by shubham on 6/23/20.
+//
+
+#include <iostream>
+#include <string>
+#include <CPISync/Syncs/GenSync.h>
+#include <CPISync/Data/DataObject.h>
+#include "TestAuxiliary.h"
+#include <stdlib.h>
+#include <CPISync/Syncs/GenSync.h>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(int argc, char *argv[]) {
+
+    ZZ_p::init(randZZ());
+
+
+    const int SEED = 93;
+    srand(SEED);
+    const int BITS = sizeof(randZZ());
+    const int NUM_EXP_ELEMS = UCHAR_MAX * 4;
+    const int PORT0 = 8001;
+//    const int PORT1 = 8002;
+    const int PORT2 = 8003;
+
+
+    // IBLTSync
+    GenSync IBLTSyncServer = GenSync::Builder().
+            setProtocol(GenSync::SyncProtocol::IBLTSync).
+            setComm(GenSync::SyncComm::socket).
+            setBits(BITS).
+            setExpNumElems(NUM_EXP_ELEMS).
+            setPort(PORT0).
+            build();
+
+    GenSync IBLTSyncClient = GenSync::Builder().
+            setProtocol(GenSync::SyncProtocol::IBLTSync).
+            setComm(GenSync::SyncComm::socket).
+            setBits(BITS).
+            setExpNumElems(NUM_EXP_ELEMS).
+            setPort(PORT0).
+            build();
+
+
+//    const long NUM_EXP_ELEMS_CHILD_SET = (rand()% 10 + 1); //This is the number of sets
+//    // IBLTSetOfSets
+//    GenSync IBLTSetOfSetsSyncServer = GenSync::Builder().
+//            setProtocol(GenSync::SyncProtocol::IBLTSetOfSets).
+//            setComm(GenSync::SyncComm::socket).
+//            setBits(BITS).
+//            setExpNumElems(NUM_EXP_ELEMS).
+//            setExpNumElemChild(NUM_EXP_ELEMS_CHILD_SET).
+//            setPort(PORT1).
+//            build();
+//
+//    GenSync IBLTSetOfSetsSyncClient = GenSync::Builder().
+//            setProtocol(GenSync::SyncProtocol::IBLTSetOfSets).
+//            setComm(GenSync::SyncComm::socket).
+//            setBits(BITS).
+//            setExpNumElems(NUM_EXP_ELEMS).
+//            setExpNumElemChild(NUM_EXP_ELEMS_CHILD_SET).
+//            setPort(PORT1).
+//            build();
+
+
+    const int mBar = 2 * UCHAR_MAX;
+    const int err = 8;
+    // CPISync
+    GenSync CPISyncServer = GenSync::Builder().
+            setProtocol(GenSync::SyncProtocol::CPISync).
+            setComm(GenSync::SyncComm::socket).
+            setBits(BITS).
+            setMbar(mBar).
+            setErr(err).
+            setHashes(true).
+            setPort(PORT2).
+            build();
+
+    GenSync CPISyncClient = GenSync::Builder().
+            setProtocol(GenSync::SyncProtocol::CPISync).
+            setComm(GenSync::SyncComm::socket).
+            setBits(BITS).
+            setMbar(mBar).
+            setErr(err).
+            setHashes(true).
+            setPort(PORT2).
+            build();
+
+    // read elements of client and server from file
+    // add elements to client and server
+
+    ifstream clientDataFile("client0.txt");
+    string temp;
+    while (getline(clientDataFile, temp, '\n')) {
+        IBLTSyncClient.addElem(make_shared<DataObject>(temp));
+//        IBLTSetOfSetsSyncClient.addElem(make_shared<DataObject>(temp));
+        CPISyncClient.addElem(make_shared<DataObject>(temp));
+    }
+
+    ifstream serverDataFile("server0.txt");
+    while (getline(serverDataFile, temp, '\n')) {
+        IBLTSyncServer.addElem(make_shared<DataObject>(temp));
+//        IBLTSetOfSetsSyncServer.addElem(make_shared<DataObject>(temp));
+        CPISyncServer.addElem(make_shared<DataObject>(temp));
+    }
+
+    // print basic stats about number of elements
+    multiset<string> initialClient;
+    for (const auto &elem : IBLTSyncClient.dumpElements()) {
+        initialClient.insert(elem);
+    }
+
+    multiset<string> initialServer;
+    for (const auto &elem : IBLTSyncServer.dumpElements()) {
+        initialServer.insert(elem);
+    }
+
+    multiset<string> resultantUnion;
+    resultantUnion = multisetUnion(initialClient, initialServer);
+
+    cout << "Initial Server size: " << initialServer.size()
+         << "\nInitial Client size: " << initialClient.size()
+         << "\nExpected Resultant size: " << resultantUnion.size() << "\n\n";
+
+
+    // run sync for IBLTSync with Multiset
+    bool isSuccess = false;
+    bool isIBLTSetSuccess = false;
+    bool isCPISuccess = false;
+    if (strcmp(argv[1], "client") == 0) {
+        cout << "IBLTSync listening on port " << PORT0 << "..." << endl;
+        isSuccess = IBLTSyncClient.clientSyncBegin(0);
+        cout << "IBLT Client stats: \n";
+        if (isSuccess) cout << "IBLTsync succeeded." << endl; else cout << "IBLTsync failed." << endl;
+        cout << "Resultant client size: " << IBLTSyncClient.dumpElements().size() << endl;
+        cout << IBLTSyncClient.printStats(0) << "\n\n";
+
+//        cout << "IBLTSetOfSetsSync listening on port " << PORT1 << "..." << endl;
+//        isIBLTSetSuccess = IBLTSetOfSetsSyncClient.clientSyncBegin(0);
+//        cout<< "IBLTSetOfSetsSync Client stats: \n";
+//        if (isIBLTSetSuccess) cout << "IBLTSetOfSetsSync succeeded." << endl; else cout << "IBLTSetOfSetsSync failed." << endl;
+//        cout << "Resultant client size: " << IBLTSetOfSetsSyncClient.dumpElements().size() << endl;
+//        cout << IBLTSetOfSetsSyncClient.printStats(0) << "\n\n";
+
+        cout << "CPISync listening on port " << PORT2 << "..." << endl;
+        isCPISuccess = CPISyncClient.clientSyncBegin(0);
+        cout << "CPISync Client stats: \n";
+        if (isCPISuccess) cout << "CPIsync succeeded." << endl; else cout << "CPIsync failed." << endl;
+        cout << "Resultant client size: " << CPISyncClient.dumpElements().size() << endl;
+        cout << CPISyncClient.printStats(0) << "\n\n";
+
+    } else {
+        cout << "IBLTSync connecting on port " << PORT0 << "..." << endl;
+        isSuccess = IBLTSyncServer.serverSyncBegin(0);
+        cout << "IBLT Server stats: \n";
+        if (isSuccess) cout << "sync succeeded." << endl; else cout << "sync failed." << endl;
+        cout << "Resultant server size: " << IBLTSyncServer.dumpElements().size() << endl;
+        cout << IBLTSyncServer.printStats(0) << "\n\n";
+
+//        cout << "IBLTSetOfSetsSync connecting on port " << PORT1 << "..." << endl;
+//        isIBLTSetSuccess = IBLTSetOfSetsSyncServer.serverSyncBegin(0);
+//        cout<< "IBLTSetOfSetsSync Server stats: \n";
+//        if (isIBLTSetSuccess) cout << "IBLTSetOfSetsSync succeeded." << endl; else cout << "IBLTSetOfSetsSync failed." << endl;
+//        cout << "Resultant server size: " << IBLTSetOfSetsSyncServer.dumpElements().size() << endl;
+//        cout << IBLTSetOfSetsSyncServer.printStats(0) << "\n\n";
+
+        cout << "CPISync connecting on port " << PORT2 << "..." << endl;
+        isCPISuccess = CPISyncServer.serverSyncBegin(0);
+        cout << "CPISync Server stats: \n";
+        if (isCPISuccess) cout << "CPIsync succeeded." << endl; else cout << "CPIsync failed." << endl;
+        cout << "Resultant server size: " << CPISyncServer.dumpElements().size() << endl;
+        cout << CPISyncServer.printStats(0) << "\n\n";
+    }
+
+    return 0;
+}
+
